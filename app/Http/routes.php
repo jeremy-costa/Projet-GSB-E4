ff0b43d63b7791b4f283564b0ffb9781ace9b1f2
Olivier <olivier.rosinski@lmdsio.fr>
2016-11-21 17:28:50 +0100

    Add files via upload

---
+++ app/Http/routes.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+
+
+Route::get('/', function () { return view('accueil'); });
+
+//Partie connexion et inscription
+Route::get('/getLogin', 'ClientController@getlogin');
+Route::get('/getSubscribe', 'ClientController@getsubscribe');
+Route::post('/login', 'ClientController@signIn');
+Route::get('/getLogout', 'ClientController@signOut');
+
