b3de3de6d95903277627d672c609857c1c97b3ec
adminlabo <adminlabo@192.168.228.146>
2016-12-05 17:34:47 +0100

    chaussures

--- resources/views/desChaussure.blade.php
+++ resources/views/desChaussure.blade.php
@@ -1,8 +1,57 @@
-<?php
+@extends('layouts.master')
+@section('content')
+<div>
+    <br> <br>
+    <br> <br>
+    <div class="container">
+        <div class="blanc">
+            <br><br>
+        </div>
+        <table class="table table-bordered table-striped">          
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+            <div class="col-md-4">
+                <div class="col-md-8">
+                    {{ $uneChaussure->LIBELLECH }}
+
+                    <a href="{{ url('/chaussure') }}/{{ $uneChaussure->IDCH }}">
+                        <img  src="../../resources/images/{{$uneChaussure->IMAGE }}"</img>   
+                    </a>
+                    {{ $uneChaussure->NOMMARQUE }}
+                    {{ $uneChaussure->PRIXCH }}
+                    <br>
+                    @if(isset($Client)) 
+                    @if ( $Client->LVLSECURITE == 1)
+                    <a href="{{ url('/modifierChaussure') }}/{{ $uneChaussure->IDCH }}/{{ $type }}">
+                        <span class="glyphicon glyphicon-pencil" data-toggle="tooltip" data-placement="top" title="Modifier"> Modifier</span></a>
+                    <br>
+                    <a class="glyphicon glyphicon-remove" data-toggle="tooltip" data-placement="top" title="Supprimer" href="{{ url('/supprimer')}}/{{  $uneChaussure->IDCH }}/{{ $type }}"
+                       onclick="javascript:if (confirm('Suppression confirmée ?'))"> Supprimer
+                    </a>                  
+                    @endif
+                    @endif
+                </div>
+
+
+                <div class="col-md-4">
+                    @if ($uneChaussure->STOCKCH !=0)
+                    <?php echo " en stock "; ?>
+                    <span class="glyphicon glyphicon-ok-sign" data-toggle="tooltip" data-placement="top"> </span>
+                    @endif
+                    @if ($uneChaussure->STOCKCH==0)
+                    <?php echo " Rupture "; ?>
+                    <span class="glyphicon glyphicon-remove-sign" data-toggle="tooltip" data-placement="top"> </span>
+                    @endif
+                    <span class="glyphicon glyphicon-ok-sign" data-toggle="tooltip" data-placement="top"> </span>
+                </div>      
+                <a href="{{ url('/ajouterPanier') }}/{{ $uneChaussure->IDCH }}" type="button" class="btn btn-success">Ajouter au panier :)</a>
+            </div>
+
+            <BR> <BR>
+        </table>
+    </div>
+</div>
+@stop
+
+
+
 
