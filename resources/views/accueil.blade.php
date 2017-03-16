3ecb4b824baf15efb6ec8aa156a09078e45fe5d1
Jeremy <Jeremy@192.168.1.14>
2017-03-16 16:36:25 +0100

    Merge origin/master
    Conflicts:
    	app/Http/Controllers/ConnexionController.php

--- resources/views/accueil.blade.php
+++ resources/views/accueil.blade.php
@@ -3,23 +3,11 @@
 <!Doctype html>
 <html lang="fr">
 
-    <body> 
+  
     <head>
         <meta name="viewport" content="width=device-width"/>
-    </head>
-
-    <body>
-
-        <h1>Bonjour</h1>
-    </body>
-   
-    <head>
-        <meta name="viewport" content="width=device-width"/>
-       
-
     </head>
 
 </html>
 
-@stop
 @stop
