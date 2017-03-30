0a93f27b49185491e42e7b8ebd648a6b77cc7915
Jeremy <Jeremy@192.168.1.14>
2017-03-30 15:00:58 +0200

    Merge origin/master
    Conflicts:
    	assets/css/Riotware.css
    	assets/css/bootstrap.css
    	nbproject/project.xml
    	resources/views/accueil.blade.php
    	resources/views/anciennesCommandes.blade.php
    	resources/views/layouts/master.blade.php

--- local/resources/views/AjouterActivite.blade.php
+++ local/resources/views/AjouterActivite.blade.php
@@ -6,49 +6,49 @@
     <head>
         <meta name="viewport" content="width=device-width"/>
     </head>
-    <h1>Ajout d'une activité</h1>
+    <h1 class="col-md-12">Ajout d'une activité</h1>
  <div class="form-horizontal col-md-12">   
                 <div class="col-md-12">
-                    <label class="col-md-3 control-label"><i class="glyphicon glyphicon-tag"> </i> Nom : </label>   
+                    <label class="col-md-6 positionEcran control-label"><i class="glyphicon glyphicon-tag"> </i> Nom : </label>   
                     <div class="col-md-6">
-                        <input type="text" name="nom" class="form-control" placeholder="Votre Nom" required autofocus>
+                        <input type="text" name="nom" class="form-control positionText" placeholder="Votre Nom" required autofocus>
                     </div>
                 </div>
                 <div class="col-md-12">
-                    <label class="col-md-3 control-label"><i class="glyphicon glyphicon-tag"> </i> Prénom : </label>
+                    <label class="col-md-6 positionEcran control-label"><i class="glyphicon glyphicon-tag"> </i> Prénom : </label>
                     <div class="col-md-6">
-                        <input type="text" name="prenom" class="form-control" placeholder="Votre Prénom" required>
+                        <input type="text" name="prenom" class="form-control positionText" placeholder="Votre Prénom" required>
                     </div>
                 </div>
                 <div class="col-md-12">
-                    <label class="col-md-3 control-label"><i class="glyphicon glyphicon-home"> </i> Adresse : </label>
+                    <label class="col-md-6 positionEcran control-label"><i class="glyphicon glyphicon-home"> </i> Adresse : </label>
                     <div class="col-md-6">
-                        <input type="text" name="adr" class="form-control" placeholder="Votre adresse" required>
+                        <input type="text" name="adr" class="form-control positionText" placeholder="Votre adresse" required>
                     </div>
                 </div>
                 <div class="col-md-12 ">
 
-                    <label class="col-md-3 control-label "> <i class="glyphicon glyphicon-earphone"> </i> Téléphone :</label>
+                    <label class="col-md-6 positionEcran control-label "> <i class="glyphicon glyphicon-earphone"> </i> Téléphone :</label>
                     <div class="col-md-6">
-                        <input type="tel" name="tel" class="form-control" placeholder="Votre téléphone">
+                        <input type="tel" name="tel" class="form-control positionText" placeholder="Votre téléphone">
                     </div>
                 </div>
                 <div class="col-md-12">
-                    <label class="col-md-3 control-label"><i class= "glyphicon glyphicon-user"> </i> Identifiant : </label>
+                    <label class="col-md-6 positionEcran control-label"><i class= "glyphicon glyphicon-user"> </i> Identifiant : </label>
                     <div class="col-md-6">
-                        <input type="text" name="login" class="form-control" placeholder="Votre identifiant" required>
+                        <input type="text" name="login" class="form-control positionText" placeholder="Votre identifiant" required>
                     </div>
                 </div>
                 <div class="col-md-12">
-                    <label class="col-md-3 control-label"><i class= "glyphicon glyphicon-eye-close"> </i> Mot de passe : </label>
+                    <label class="col-md-6 positionEcran control-label"><i class= "glyphicon glyphicon-eye-close"> </i> Mot de passe : </label>
                     <div class="col-md-6">
-                        <input type="password" name="pwd" class="form-control" placeholder="Votre mot de passe" required>
+                        <input type="password" name="pwd" class="form-control positionText" placeholder="Votre mot de passe" required>
                     </div>
                 </div>
                 <div class="col-md-12">
-                    <label class="col-md-3 control-label"><i class="glyphicon glyphicon-envelope"> </i> Adresse e-mail : </label>
+                    <label class="col-md-6 positionEcran control-label"><i class="glyphicon glyphicon-envelope"> </i> Adresse e-mail : </label>
                     <div class="col-md-6">
-                        <input type="email" name="mail" class="form-control" placeholder="Votre adresse e-mail" required>
+                        <input type="email" name="mail" class="form-control positionText" placeholder="Votre adresse e-mail" required>
                     </div>
                 </div>
 
